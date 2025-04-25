da8d63bc76fd289a3158db3a5b8e22e63627c036
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-04-25 17:28:05 +0700

    Add project files.

---
+++ Project2/toan.c
@@ -0,0 +1,19 @@
+﻿#include "toan.h"
+int cong(int a, int b) {
+	return a + b;
+}
+
+int tru(int a, int b) {
+	return a - b;
+}
+
+int nhan(int a, int b) {
+	return a * b;
+}
+
+float chia(int a, int b) {
+	if (b == 0) {
+		return 0; // Trả về 0 nếu chia cho 0
+	}
+	return (float)a / b;
+}
