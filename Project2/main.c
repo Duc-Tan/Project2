6c71e5560cc85a3d682d76739e243c9cac136c9d
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-05-03 18:46:07 +0700

    hinh tam giac nguoc

--- Project2/main.c
+++ Project2/main.c
@@ -8,8 +8,8 @@
 void main() {
 	int n = 4;
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n - i; j++) {
-			if (i == 0 || j == 0 || j == n - i - 1)
+		for (int j = 0; j <= i; j++) {
+			if (j == 0 || i == n-1 || j == i)
 				printf("*");
 			else
 				printf(" ");
